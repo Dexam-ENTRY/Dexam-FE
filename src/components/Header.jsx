fac7f3405c0bd7557f26a747188ce9df30cb6e4a
syoon1 <syoon12356@gmail.com>
2026-08-12 00:25:00 +0900

    feat: Header 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 import { colors } from "../design-token";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
-export const Header = ({ title, Color = colors.gray[600] }) => {
+export const Header = ({ title, modeText, Color = colors.gray[600] }) => {
   return (
     <HeaderWrapper>
-      <Title $Color={Color}>{title}</Title>
-      <Mode>
-        <Circle>
-          <></>
-        </Circle>
-      </Mode>
+      <IconWrapper>
+        <FaArrowLeftLong />
+        <Title $Color={Color}>{title}</Title>
+      </IconWrapper>
+      <ModeWrapper>
+        <Mode>
+          <Circle></Circle>
+          <ModeText>{modeText}</ModeText>
+        </Mode>
+      </ModeWrapper>
     </HeaderWrapper>
   );
 };
@@ -17,29 +22,55 @@ export const Header = ({ title, Color = colors.gray[600] }) => {
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
   background-color: #fffcf3;
-  outline-color: #ffffff;
-  margin: 25px 80px;
-  gap: 8px;
+  border: 1px solid #ffffff;
   position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
+  top: 25px;
+  left: 80px;
+  right: 80px;
   z-index: 1000;
+  height: auto;
+`;
+
+const IconWrapper=styled.div`
+  display:flex;
+  align-items:center;
+  gap:10px;
+`
+
+const ModeWrapper = styled.div`
+  display: flex;
+  align-items: start;
 `;
 
 const Title = styled.div`
-  font-size: 32px;
+  font-size: 20px;
   font-weight: bold;
   color: ${({ $Color }) => $Color};
+  display: flex;
 `;
 
 const Mode = styled.div`
-  color: #ffffff;
+  background-color: #ffffff;
   width: 140px;
   height: 50px;
   border-radius: 30px;
+  border: 1px solid #dce5de;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding-left: 20px;
 `;
 
-const Circle = styled.div``;
+const Circle = styled.div`
+  border-radius: 10px;
+  width: 20px;
+  height: 20px;
+  background-color: #087a43;
+`;
+
+const ModeText = styled.div`
+  font-size: 15px;
+  color: #1f2925;
+  padding-left: 10px;
+`;
